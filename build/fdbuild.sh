75a25ac3e67ccf43efbb9c1c626b321edbb781e1
BenBouiss <benjamin.bouissou@gmail.com>
2025-11-30 23:54:40 +0100

    [MISC] debug build script

---
+++ build/fdbuild.sh
@@ -0,0 +1,12 @@
+
+
+#me="$(whoami)"
+me="$USER"
+tmp_folder="/home/$me/.zig-tmp"
+mkdir -p "$tmp_folder"
+echo "Creating temporary folder at: $tmp_folder"
+
+export ZIG_LOCAL_CACHE_DIR="$tmp_folder/.zig-cache"
+export ZIG_GLOBAL_CACHE_DIR="$tmp_folder/.zig-cache"
+
+zig build -Doptimize=ReleaseFast -DfastBitscan=true -DuseMagic=true -DuseDebug=true
