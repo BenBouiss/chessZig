c684976555486801be43c9cfdc4a526a96becfba
BenBouiss <benjamin.bouissou@gmail.com>
2026-05-29 18:22:33 +0200

    [FEAT] more compact TT repr

--- src/hashTable.zig
+++ src/hashTable.zig
@@ -115,7 +115,7 @@ pub const probeResult = struct {
     entry: ?Hash_entry = .{},
 };
 pub const hashWriter = struct {
-    bucket: *Hash_bucket = undefined,
+    bucket: Hash_bucket = undefined,
     idx: u8 = 0,
 
     pub inline fn init(key: u64) hashWriter {
@@ -134,16 +134,22 @@ pub const hashWriter = struct {
     }
 };
 pub const Hash_bucket = struct {
-    entries: [configl.ITEM_PER_BUCKET]Hash_entry = undefined,
-    len: u8 = 0,
+    entries: [*]Hash_entry = undefined,
 
-    pub fn printSize(p_self: *Hash_bucket) void {
+    pub fn printSize(p_self: *const Hash_bucket) void {
         std.debug.print("[DEBUG] printSize: hash bucket = {d} bytes\n", .{@sizeOf(Hash_bucket)});
         std.debug.print("[DEBUG] printSize: entries size is {d} bytes\n", .{@sizeOf(Hash_entry)});
         std.debug.print("[DEBUG] printSize: entries val size is {d} bytes\n", .{@sizeOf(entryComponents)});
         std.debug.print("[DEBUG] printSize: is of perft entry is {d} bytes\n", .{@sizeOf(perftEntry)});
         std.debug.print("[DEBUG] printSize: is of search entry is {d} bytes\n", .{@sizeOf(searchEntry)});
         _ = p_self;
+    }
+    pub fn len(self: Hash_bucket) u8 {
+        var ret: u8 = 0;
+        for (0..configl.ITEM_PER_BUCKET) |i| {
+            ret += @intFromBool(self.entries[i]._valid);
+        }
+        return ret;
     }
     pub fn addEntry(p_self: *Hash_bucket, entry: Hash_entry, strategy: TT_strat) bool {
         switch (strategy) {
@@ -169,7 +175,7 @@ pub const Hash_bucket = struct {
             const currDepth = entry._depth;
             if (!entry.valid() or (entry.age() + configl.OLD_THRESHOLD) < n_entry.age()) {
                 p_self.entries[i] = n_entry;
-                p_self.len = @min(p_self.len + 1, p_self.entries.len);
+                //p_self.len = @min(p_self.len + 1, p_self.entries.len);
                 return true;
             }
             if (entry.key == n_entry.key) {
@@ -199,7 +205,7 @@ pub const Hash_bucket = struct {
             const _age = _entry.age();
             if (!_entry.valid() or (_age + configl.SCHEDULER_MAX_ENDGAME_DEPTH) < n_entry.age()) {
                 p_self.entries[i] = n_entry;
-                p_self.len = @min(p_self.len + 1, p_self.entries.len);
+                //p_self.len = @min(p_self.len + 1, p_self.entries.len);
                 return true;
             }
             if (_entry.key == n_entry.key) {
@@ -220,13 +226,15 @@ pub const Hash_bucket = struct {
         //p_self.len = @min(p_self.len + 1, p_self.entries.len);
     }
     pub fn addEntry_AR(p_self: *Hash_bucket, entry: Hash_entry) bool {
-        p_self.entries[p_self.len] = entry;
-        p_self.len = (p_self.len + 1) % configl.ITEM_PER_BUCKET;
+        _ = p_self;
+        _ = entry;
+        //p_self.entries[p_self.len] = entry;
+        //p_self.len = (p_self.len + 1) % configl.ITEM_PER_BUCKET;
         return true;
     }
-    pub fn getEntryPerft(p_self: *Hash_bucket, hash: u64, depth: u8) ?Hash_entry {
+    pub fn getEntryPerft(p_self: *const Hash_bucket, hash: u64, depth: u8) ?Hash_entry {
         const _hash = keyToUpperKey(hash);
-        for (0..p_self.len) |i| {
+        for (0..configl.ITEM_PER_BUCKET) |i| {
             const entry = p_self.entries[i];
             if (entry.key == _hash and entry._depth == depth) {
                 return entry;
@@ -234,7 +242,7 @@ pub const Hash_bucket = struct {
         }
         return null;
     }
-    pub fn getEntryMatchNext(p_self: *Hash_bucket, hash: u64, depth: u8) getResult {
+    pub fn getEntryMatchNext(p_self: *const Hash_bucket, hash: u64, depth: u8) getResult {
         const _hash = keyToUpperKey(hash);
         var next: u8 = 0;
         var nextA: u8 = 255;
@@ -277,7 +285,7 @@ pub const hashTableStat = struct {
     insertion: u64 = 0,
 };
 pub const Hash_table = struct {
-    entries: []Hash_bucket,
+    entries: []Hash_entry,
     MBsize: u32 = 0,
     closestBit: u8 = 0,
     size: u64 = 0,
@@ -291,8 +299,9 @@ pub const Hash_table = struct {
         var ret: Hash_table = undefined;
         ret.MBsize = MBsize;
 
-        var total_size: u64 = @intCast(MBsize * 1000000);
-        total_size = @divFloor(total_size, @sizeOf(Hash_bucket));
+        //var total_size: u64 = @intCast(MBsize * 1000000);
+        var total_size: u64 = @intCast(MBsize * 1024 * 1024);
+        total_size = @divFloor(total_size, @sizeOf(Hash_entry) * configl.ITEM_PER_BUCKET);
 
         ret.closestBit = chess.l_getMsbIdx(total_size);
         ret.size = chess.xToBitboard(ret.closestBit);
@@ -300,21 +309,28 @@ pub const Hash_table = struct {
         ret.stat.insertion = 0;
         ret.gen = 0;
 
-        ret.entries = (try alloc.alloc(Hash_bucket, ret.size));
+        ret.entries = (try alloc.alloc(Hash_entry, ret.size * configl.ITEM_PER_BUCKET));
 
         for (0..ret.size) |i| {
-            ret.entries[i].len = 0;
+            var b = ret.getBucket(@intCast(i));
             for (0..configl.ITEM_PER_BUCKET) |j| {
-                ret.entries[i].entries[j] = .{ .val = .{ .search = .{} } };
+                b.entries[j] = .{ .val = .{ .search = .{} } };
             }
+            //ret.entries[i] = .{ .val = .{ .search = .{} } };
+
+            //var b: [*]Hash_entry = &ret.entries[i];
         }
         ret.initialized = true;
 
         if (verbose) {
-            std.debug.print("[PRE] Initializing hash table with a size of {d} buckets closest bit {d}!\n", .{ ret.size, ret.closestBit });
-            ret.entries[0].printSize();
+            std.debug.print("[PRE] Initializing hash table with a size of {d} buckets closest bit {d} for input of {d}MB = {d} msb total size {d}! Size of one bucket {d}\n", .{ ret.size, ret.closestBit, MBsize, chess.l_getMsbIdx(total_size), total_size, @sizeOf(Hash_bucket) });
+            ret.getBucket(0).printSize();
         }
         return ret;
+    }
+    pub inline fn getBucket(p_self: *Hash_table, bucketIdx: u64) Hash_bucket {
+        const base = bucketIdx * configl.ITEM_PER_BUCKET;
+        return .{ .entries = @as([*]Hash_entry, @ptrCast(p_self.entries)) + base };
     }
     pub fn free(p_self: *Hash_table, alloc: std.mem.Allocator, verbose: bool) void {
         if (verbose) {
@@ -334,15 +350,16 @@ pub const Hash_table = struct {
     pub inline fn getHashIndex(self: Hash_table, hash: u64) u64 {
         return hash & self.mask;
     }
-    pub inline fn getBucketFromFullHashIndex(self: Hash_table, hash: u64) *Hash_bucket {
+    pub inline fn getBucketFromFullHashIndex(self: *Hash_table, hash: u64) Hash_bucket {
         const index = self.getHashIndex(hash);
-        return &self.entries[index];
+        return self.getBucket(index);
+        //return &self.entries[index];
     }
 
-    pub fn overwriteEvaluationEntries(p_self: *Hash_table, p_entry: *const Hash_entry, score: scoreType) void {
+    pub fn overwriteEvaluationEntries(p_self: *Hash_table, p_entry: *Hash_entry, score: scoreType) void {
         const index = p_entry.key;
         var p_bucket = p_self.getBucketFromFullHashIndex(index);
-        for (0..p_bucket.len) |i| {
+        for (0..configl.ITEM_PER_BUCKET) |i| {
             var ent = &p_bucket.entries[i];
             if (ent.key == p_entry.key) {
                 ent.val.search.evaluation = score;
@@ -357,11 +374,11 @@ pub const Hash_table = struct {
         }
         return true;
     }
-    pub inline fn probePerft(p_self: *const Hash_table, key: u64, depth: u8) probeResult {
+    pub inline fn probePerft(p_self: *Hash_table, key: u64, depth: u8) probeResult {
         const p_bucket = p_self.getBucketFromFullHashIndex(key);
         return .{ .writer = .{ .bucket = p_bucket, .idx = 0 }, .entry = p_bucket.getEntryPerft(key, depth) };
     }
-    pub fn probeMatch(p_self: *const Hash_table, key: u64, depth: u8) probeResult {
+    pub fn probeMatch(p_self: *Hash_table, key: u64, depth: u8) probeResult {
         const p_bucket = p_self.getBucketFromFullHashIndex(key);
         const res = p_bucket.getEntryMatchNext(key, depth);
         return .{ .writer = .{ .bucket = p_bucket, .idx = res.nextIdx }, .entry = res.entry };
@@ -376,24 +393,24 @@ pub const Hash_table = struct {
     }
     pub fn countNonEmpty(p_self: *Hash_table) u64 {
         var ret: u64 = 0;
-        for (p_self.entries) |e| {
-            ret += @intFromBool(e.len != 0);
+        for (0..p_self.size) |i| {
+            ret += @intFromBool(p_self.getBucket(i).len() != 0);
         }
         return ret;
     }
     pub fn countValids(p_self: *Hash_table) u64 {
         var ret: u64 = 0;
         for (p_self.entries) |e| {
-            ret += @intCast(e.len);
+            ret += @intFromBool(e._valid);
         }
         return ret;
     }
 
     pub fn getMostUtilized(p_self: *Hash_table) u8 {
         var ret: u8 = 0;
-        for (0..p_self.entries.len) |i| {
-            const e = p_self.entries[i];
-            ret = @max(ret, e.len);
+        for (0..p_self.size) |i| {
+            const e = p_self.getBucket(@intCast(i));
+            ret = @max(ret, e.len());
             if (ret == configl.ITEM_PER_BUCKET) {
                 break;
             }
@@ -403,11 +420,11 @@ pub const Hash_table = struct {
 };
 
 pub fn getEntryFromPerft(key: Key, depth: u8) ?Hash_entry {
-    const p_bucket: *Hash_bucket = hashTable.getBucketFromFullHashIndex(key.code);
+    const p_bucket: Hash_bucket = hashTable.getBucketFromFullHashIndex(key.code);
     return p_bucket.getEntryPerft(key.code, depth);
 }
 pub inline fn getEntryFromMatch(key: Key, depth: u8) ?Hash_entry {
-    const p_bucket: *Hash_bucket = hashTable.getBucketFromFullHashIndex(key.code);
+    var p_bucket: Hash_bucket = hashTable.getBucketFromFullHashIndex(key.code);
     return p_bucket.getEntryMatch(key.code, depth);
 }
 
@@ -503,11 +520,11 @@ pub inline fn updateKey(keyDst: *Key, keySrc: Key) void {
 
 pub fn printTTStats() void {
     const n = hashTable.countNonEmpty();
-    const frac: f64 = @as(f64, @floatFromInt(n)) / @as(f64, @floatFromInt(hashTable.entries.len)) * 100;
-    std.log.info("TT: {d:.2}% of buckets used, non empty {d} total {} buckets", .{ frac, n, hashTable.entries.len });
+    const frac: f64 = @as(f64, @floatFromInt(n)) / @as(f64, @floatFromInt(hashTable.size)) * 100;
+    std.log.info("TT: {d:.2}% of buckets used, non empty {d} total {} buckets", .{ frac, n, hashTable.size });
 
     const nvalid = hashTable.countValids();
-    const frac2: f64 = @as(f64, @floatFromInt(nvalid)) / @as(f64, @floatFromInt(hashTable.entries.len * configl.ITEM_PER_BUCKET)) * 100;
+    const frac2: f64 = @as(f64, @floatFromInt(nvalid)) / @as(f64, @floatFromInt(hashTable.entries.len)) * 100;
     std.log.info("TT: total utilization {d:.2}%", .{frac2});
 
     const util = hashTable.getMostUtilized();
